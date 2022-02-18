cc7c9c3400038620320e8aec2e4d89e0d4d57c08
Grant Harris <grant.harris@ibm.com>
2022-02-18 16:32:07 -0600

    Set up reducer and alert

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,26 +1,32 @@
-import React, {useState, useReducer, useContext} from 'react'
+import React, { useReducer, useContext } from 'react';
+
+import reducer from './reducer';
+
+import { DISPLAY_ALERT } from './actions';
 
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: '',
   alertType: '',
-}
+};
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
-  const [state, setState] = useState(initialState);
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const displayAlert = () => {
+    dispatch({type: DISPLAY_ALERT})
+  }
 
   return (
-    <AppContext.Provider value={{...state}}>
-      {children}
-    </AppContext.Provider>
-  )
-}
+    <AppContext.Provider value={{ ...state, displayAlert }}>{children}</AppContext.Provider>
+  );
+};
 
 const useAppContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
 export { AppProvider, initialState, useAppContext };
